15e6b532f93f23f762ad7832071948e4b6b1db46
Winford <69995513+UncleGrumpy@users.noreply.github.com>
2021-06-06 15:36:49 -0700

    Changed the way strings are created for mqtt messages. Removed OTA funtions.
    The OTA funtions were removed beacuse that method of OTA will only work with
    an always on device. Another mothod of updating will have to be developed.
    The preparatioin of data strings to be tranmitted over MQTT are now much cleaner,
    and more efficient - however all of the MQTT handling is likely to be revised in
    the near future. For privacy most of the user defined variable have been moved to
    conf.h, this includes SSID, PASSWORD, and MQTT_IP.

--- ESP-SolMon.ino
+++ ESP-SolMon.ino
@@ -1,19 +1,28 @@
 // BMx280_I2C.ino
 //
-// shows how to use the BMP280 / BMx280 library with the sensor connected using I2C.
+// shows how to use the BMx280 library with the sensor connected using I2C.
 //
 // Copyright (c) 2018 Gregor Christandl
 
 /***************************************************************************
 * Example sketch for the ADS1115_WE library
 *
-* This sketch shows how to use the ADS1115 in single shot mode. 
-*  
+* This sketch shows how to use the ADS1115 in single shot mode.
+*
 * Further information can be found on:
 * https://wolles-elektronikkiste.de/ads1115 (German)
 * https://wolles-elektronikkiste.de/en/ads1115-a-d-converter-with-amplifier (English)
-* 
+*
 ***************************************************************************/
+//
+// ESP-SolMon
+//
+// Esp-12E weather monitor featuring bme280 for climate measurements, ads1115
+// to measure analog readings from the amazing open hardware
+// https://github.com/fadushin/solar-esp32/ solar power LiFePo4 battery charge supervisor.
+//
+// Copyright (c) 2021 Winford (Uncle Grumpy)
+//
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -27,11 +36,13 @@
 #include <Hash.h>
 #include <include/WiFiState.h>
 #include <coredecls.h>         // crc32()
+#include <ArduinoJson.h>
+#include "conf.h"             // create this file and #define SSID, PASSWORD, MQTT_IP... 
+                              // or comment out the above line and fill in below.
 
-
-const char* ssid = "SSID;
-const char* password = "PASSWD";
-const char* mqtt_server = "IP address";
+const char* ssid = SSID;
+const char* password = PASSWORD;
+const char* mqtt_server = MQTT_IP;
 
 #define BMP_ADDRESS 0x76
 #define ADC_ADDRESS 0x48
@@ -41,16 +52,15 @@ const char* mqtt_server = "IP address";
 //create an BMP280 object using the I2C interface
 BMx280I2C bmx280(BMP_ADDRESS);
 
-//create an ADS1115 object using the I2C interface 
+//create an ADS1115 object using the I2C interface
 ADS1115_WE adc = ADS1115_WE(ADC_ADDRESS);
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-
 float espVoltage;   //BAD global! this is my lazy way of reding the voltage before wifi is active
 ADC_MODE(ADC_VCC);
 
-struct nv_s {
+struct nv_s {     // this trick comes from the esp8268/LowPowerDemo example sketch.
   WiFiState wss; // core's WiFi save state
 
   struct {
@@ -75,8 +85,8 @@ void setup() {
   }
   nv->rtcData.rstCount = resetCount; // update the reset count & CRC
   updateRTCcrc();
-  
-	Serial.begin(115200);
+
+  Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println();
@@ -86,8 +96,8 @@ void setup() {
   pinMode(4, OUTPUT);       // base of 2N2222 peripheral power control.
   digitalWrite(4, HIGH);    // Power on peripherals.
 
-	Wire.begin(SDA,SCL);
-  
+  Wire.begin(SDA,SCL);
+
   /* SETUP BMP */
   //begin() checks the Interface, reads the sensor ID (to differentiate between BMP280 and BME280)
   //and reads compensation parameters.
@@ -102,7 +112,7 @@ void setup() {
 
   //by default sensing is disabled and must be enabled by setting a non-zero
   //oversampling setting.
-  //set an oversampling setting for pressure and temperature measurements. 
+  //set an oversampling setting for pressure and temperature measurements.
   bmx280.writeOversamplingPressure(BMx280MI::OSRS_P_x16);
   bmx280.writeOversamplingTemperature(BMx280MI::OSRS_T_x16);
 
@@ -117,7 +127,7 @@ void setup() {
 
   /* Set the voltage range of the ADC to adjust the gain
    * Please note that you must not apply more than VDD + 0.3V to the input pins!
-   * 
+   *
    * ADS1115_RANGE_6144  ->  +/- 6144 mV
    * ADS1115_RANGE_4096  ->  +/- 4096 mV
    * ADS1115_RANGE_2048  ->  +/- 2048 mV (default)
@@ -133,28 +143,28 @@ void setup() {
   //adc.setCompareChannels(ADS1115_COMP_2_GND);   /* uncomment to enable */
   //adc.setCompareChannels(ADS1115_COMP_3_GND);   /* uncomment to enable */
 
-  /* 
-   * Alert pin is NOT connected!!! 
+  /*
+   * Alert pin is NOT connected!!!
    *    Do not enable it!!!
    */
   adc.setAlertPinMode(ADS1115_DISABLE_ALERT);
 
   /* Set the conversion rate in SPS (samples per second)
-   * Options should be self-explaining: 
-   * 
-   *  ADS1115_8_SPS 
-   *  ADS1115_16_SPS  
-   *  ADS1115_32_SPS 
-   *  ADS1115_64_SPS  
+   * Options should be self-explaining:
+   *
+   *  ADS1115_8_SPS
+   *  ADS1115_16_SPS
+   *  ADS1115_32_SPS
+   *  ADS1115_64_SPS
    *  ADS1115_128_SPS (default)
-   *  ADS1115_250_SPS 
-   *  ADS1115_475_SPS 
-   *  ADS1115_860_SPS 
+   *  ADS1115_250_SPS
+   *  ADS1115_475_SPS
+   *  ADS1115_860_SPS
    */
   adc.setConvRate(ADS1115_860_SPS); //uncomment if you want to change the default
 
   /* Set continuous or single shot mode:
-   * 
+   *
    *  ADS1115_CONTINUOUS  ->  continuous mode
    *  ADS1115_SINGLE     ->  single shot mode (default)
    */
@@ -168,9 +178,9 @@ void setup() {
     delay(100);
     Serial.print(".");
   }
-  
+
  client.setServer(mqtt_server, 1883);
- 
+
 }
 
 void loop() {
@@ -180,8 +190,7 @@ void loop() {
   client.loop();
   reportClimate();
   reportPower();
-  readVoltage();
-  handleOTA();
+  reportVoltage();
   digitalWrite(4, LOW);    // Power off peripherals.
   reportWaketime();        // report time spent awake.
   ESP.deepSleep(60e6);
@@ -189,24 +198,30 @@ void loop() {
 
 void reconnect() {
   // Loop until we're reconnected
-  using namespace std;
   while (!client.connected()) {
+    char message[64];
+    char counter[9];
+    itoa ((int)resetCount,counter,10);
+    StaticJsonDocument<48> doc;
+    doc["count"] = counter;
+    serializeJson(doc, message);
     Serial.println();
     Serial.print("Connection to MQTT broker ");
     // Attempt to connect
     if (client.connect("SolMon")) {
       Serial.println("established.");
-      //Serial.println();
       // Once connected, publish an announcement...
-      client.publish("SolMon/run", String(resetCount).c_str());
+      while (!client.publish("SolMon/run", message, true)) {
+        delay(5);
+      }
       Serial.print(F("\nReset count = "));
       Serial.println(resetCount);
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 1 second");
-      // Wait 1 second before retrying
-      delay(1000);
+      // Wait 1/2 second before retrying
+      delay(500);
     }
   }
 }
@@ -220,76 +235,21 @@ float readChannel(ADS1115_MUX channel) {
   return voltage;
 }
 
-void setupOTA() {
-  // Port defaults to 8266
-  // ArduinoOTA.setPort(8266);
-
-  // Hostname defaults to esp8266-[ChipID]
-  ArduinoOTA.setHostname("solmon");
-
-  // No authentication by default
-  //ArduinoOTA.setPassword("admin");
-
-  // Password can be set with it's md5 value as well
-  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
-  ArduinoOTA.setPasswordHash("B43B1C8BA7E211D86B90C354A60D96C3");
-
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-    client.publish("SolMon/OTA", String(type).c_str());
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-    client.publish("SolMon/OTA", "COMPLETE");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    client.publish("SolMon/OTA", String(error).c_str());
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-  Serial.println("OTA Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  client.publish("SolMon/OTA", "READY");
-}
-
-void handleOTA() {
-  setupOTA();
-  ArduinoOTA.handle();
-}
-
 void reportClimate() {
   /* MEASURE MBP280 */
   float pascals = 0.0;
-  float celsius = 0.0;
+  char message[24];
+  char value[12];
+  StaticJsonDocument<48> doc;
   
   //start a measurement
   if (!bmx280.measure())
   {
     Serial.println("could not start measurement, is a measurement already running?");
-    client.publish("SolMon/console", "could not start measurement, is a BMx280 request already running?");
+    StaticJsonDocument<48> doc;
+    doc["ERROR"] = "BMx280 Failure.";
+    serializeJson(doc, message);
+    client.publish("SolMon/console", message, true);
     return;
   }
 
@@ -298,31 +258,58 @@ void reportClimate() {
   //make sure to call get*() functions only after hasValue() has returned true.
   do
   {
-    delay(10);
+    delay(5);
   } while (!bmx280.hasValue());
 
   Serial.print("Pressure: "); Serial.println(bmx280.getPressure64());
   Serial.print("Temperature: "); Serial.println(bmx280.getTemperature());
-  
+
   // Publish to mqtt
   pascals = bmx280.getPressure64();
   float mbars = pascals / 100;
-  client.publish("climate/outdoor/millibars", String(mbars).c_str());
-  float inHg =   roundf((pascals / 3386.39) * 100) / 100;
-  client.publish("climate/outdoor/inHg", String(inHg).c_str());
-  celsius = bmx280.getTemperature();
-  float tempC = roundf(celsius * 100) / 100;
-  client.publish("climate/outdoor/temp/c", String(tempC).c_str());
-  float tempF = roundf(((celsius * 1.8) + 32 ) * 100 ) /100;
-  client.publish("climate/outdoor/temp/f", String(tempF).c_str());
+  dtostrf(mbars, 7, 2, value);  // convert mbars to 7 character value[] - rounding at 2 decimal spaces (i.e. "1013.37")
+  doc.clear();
+  doc["mbars"] = value;
+  serializeJson(doc, message);
+  while (!client.publish("sensors/climate/outdoor/millibars", message, true)) {
+    delay(5);
+  }
+  float inHg = mbars / 33.8639;
+  dtostrf(inHg, 5, 2, value);  // convert inHg to 5 character value[] - rounding at 2 decimal spaces (i.e. "29.37")
+  doc.clear();
+  doc["inHg"] = value;
+  serializeJson(doc, message);
+  while (!client.publish("sensors/climate/outdoor/inHg", message, true)) {
+    delay(5);
+  }
+  float celsius = bmx280.getTemperature();
+  dtostrf(celsius, 5, 2, value);  // convert celsius to 5 character value[] - rounding at 2 decimal spaces (i.e. "48.00")
+  doc.clear();
+  doc["C"] = value;
+  serializeJson(doc, message);
+  while (!client.publish("sensors/climate/outdoor/temp/c", message, true)) {
+    delay(5);
+  }
+  float tempF = (celsius * 1.8) + 32;
+  dtostrf(tempF, 5, 2, value);  // convert tempF to 6 character value[] - rounding at 2 decimal spaces (i.e. "100.00")
+  doc.clear();
+  doc["F"] = value;
+  serializeJson(doc, message);
+  while (!client.publish("sensors/climate/outdoor/temp/f", message, true)) {
+    delay(5);
+  }
 
   if (bmx280.isBME280())
   {
     float humid = bmx280.getHumidity();
-    Serial.print("Humidity: "); 
-    //Serial.println(humid);
-    //float humidity = roundf(humid * 100) / 100;
-    client.publish("climate/outdoor/humidity", String(humid).c_str());
+    dtostrf(humid, 5, 2, value);  // convert humid to 5 character value[] - rounding at 2 decimal spaces (i.e. "100.00")
+    doc.clear();
+    doc["percent"] = value;
+    serializeJson(doc, message);
+    Serial.print("Humidity: ");
+    while (!client.publish("sensors/climate/outdoor/humidity", message, true)) {
+      delay(5);
+    }
     Serial.println(humid);
   }
 }
@@ -330,34 +317,53 @@ void reportClimate() {
 void reportPower() {
   /* MEASURE ADS1115 */
   float voltage = 0.0;
-  float volts = 0.0;
+  //float volts = 0.0;
+  char message[24];
+  char value[6];
   
   Serial.print("Battery: ");
-  voltage = readChannel(ADS1115_COMP_0_GND);
-  //Serial.println(voltage);
-  volts = roundf(voltage * 326) / 100;
+  voltage = (readChannel(ADS1115_COMP_0_GND) * 3.26);
+  dtostrf(voltage, 4, 2, value);  // convert volts to 4 character value[] - rounding at 2 decimal spaces
+  StaticJsonDocument<48> doc;
+  doc["volts"] = value;
+  serializeJson(doc, message);
   // Publish a message to mqtt
-  client.publish("SolMon/battery", String(volts).c_str());
-  Serial.println(volts);
+  while (!client.publish("SolMon/battery", message, true)) {
+    delay(5);
+  }
+  Serial.println(value);
 
   Serial.print("Panel:   ");
-  voltage = readChannel(ADS1115_COMP_1_GND);
-  //Serial.println(voltage);
-  volts = roundf(voltage * 400) / 100;
+  voltage = (readChannel(ADS1115_COMP_1_GND) * 4);
+  dtostrf(voltage, 4, 2, value);  // convert volts to 4 character value[] - rounding at 2 decimal spaces
+  doc.clear();
+  doc["volts"] = value;
+  serializeJson(doc, message);
   // Publish a message to mqtt
-  client.publish("SolMon/panel", String(volts).c_str());
-  Serial.println(volts);
+  while (!client.publish("SolMon/panel", message, true)) {
+    delay(5);
+  }
+  Serial.println(value);
 }
 
 
-void readVoltage() { // read internal VCC
-  //float voltage = ESP.getVcc();
-  //Serial.printf("The internal VCC reads %1.2f volts\n", voltage / 1000);
-  float volts = roundf(espVoltage * .1) / 100;
+void reportVoltage() {            // read internal VCC
+  static float volts = ((espVoltage - 220) / 1000);
+  char message[24];
+  char value[6];
+  dtostrf(volts, 4, 2, value);  // convert volts to 4 character value[] - rounding at 2 decimal spaces
+  StaticJsonDocument<48> doc;
+  doc.clear();
+  doc["volts"] = value;
+  serializeJson(doc, message);
   // Publish a message to mqtt
-  client.publish("SolMon/vcc", String(volts).c_str());
-  Serial.printf("ESP Vcc:  %1.2f volts\n", volts);
-
+  while (!client.publish("SolMon/vcc", message, true)) {
+    delay(5);
+  }
+  strcat(value, "\0");
+  Serial.print("ESP Vcc: ");
+  Serial.print(value);
+  Serial.println(" volts");
 }
 
 void reportWaketime() {
@@ -365,9 +371,16 @@ void reportWaketime() {
   Serial.println(millis());
   Serial.flush();
   int wakeTime = millis();
-  while (!client.publish("SolMon/duration", String(wakeTime).c_str())) {
-    delay(10);
-  }  
+  char message[64];
+  char timer[9];
+  itoa ((int)wakeTime,timer,10);
+  StaticJsonDocument<48> doc;
+  doc["milliseconds"] = timer;
+  serializeJson(doc, message);
+  while (!client.publish("SolMon/duration", message, true)) {
+    delay(5);
+  }
+  client.disconnect();
 }
 
 void updateRTCcrc() {  // updates the reset count CRC
