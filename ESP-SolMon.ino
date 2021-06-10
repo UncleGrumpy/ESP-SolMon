560212328906afea7ebec8dd4bf72ae370b2c947
Winford <69995513+UncleGrumpy@users.noreply.github.com>
2021-06-10 02:21:19 -0700

    Support for InfluxDB added.
    InfluxDB can be selected as an output option in conf.h

--- ESP-SolMon.ino
+++ ESP-SolMon.ino
@@ -43,8 +43,6 @@ const char* mqtt_server = MQTT_IP;  // defined in conf.h
 
 #define BMP_ADDRESS 0x76
 #define ADC_ADDRESS 0x48
-//#define SDA 2
-//#define SCL 14
 
 //create an BMP280 object using the I2C interface
 BMx280I2C bmx280(BMP_ADDRESS);
@@ -74,6 +72,28 @@ HASensor sm_f("F");
 HASensor sm_humid("Humidity");
 #endif
 
+#if defined(OUTPUT_INFLUX)
+#include <InfluxDbClient.h>
+// InfluxDB client instance for InfluxDB 1
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+/* Uncomment below to use InfluxDB 1 authentication */
+//client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASSWORD);
+
+// Data point
+Point wifi_sensor("wifi_signal");
+Point run_sensor("run_counter");
+Point vcc_sensor("internal_voltage");
+Point timer_sensor("time_awake");
+Point battery_sensor("battery_voltage");
+Point panel_sensor("panel_voltage");
+Point log_sensor("console_log");
+Point mbar_sensor("outdoor_millibars");
+Point mercury_sensor("inches_mercury");
+Point celsius_sensor("outdoor_celsius");
+Point temp_sensor("outdoor_fahrenheit");
+Point humid_sensor("outdoor_humidity");
+#endif
+
 float espVoltage;   //BAD global! this is my lazy way of reding the voltage before wifi is active
 ADC_MODE(ADC_VCC);
 
@@ -89,13 +109,6 @@ struct nv_s {     // this trick comes from the esp8268/LowPowerDemo example sket
 
 static nv_s* nv = (nv_s*)RTC_USER_MEM; // user RTC RAM area
 uint32_t resetCount = 0;  // keeps track of the number of Deep Sleep tests / resets
-
-//
-/*
- * Results in:
- *    $<base-topic>/sensors/$<device-UUID>/$<HASensor>/state
- *    the "state" topic will contain the sensor value
- */
 
 void setup() {
   resetCount = 0;
@@ -201,7 +214,7 @@ void setup() {
   // Unique ID must be set!
   byte mac[WL_MAC_ADDR_LENGTH];
   WiFi.macAddress(mac);
-  #endif
+  #endif  
   
   while (WiFi.status() != WL_CONNECTED) {
     delay(100);
@@ -233,7 +246,7 @@ void setup() {
   sm_sol.setIcon("mdi:solar-power");
   sm_sol.setDeviceClass("voltage");
   sm_sol.setUnitOfMeasurement("V");
-  sm_wifi.setName("ESP WiFi Signal");
+  sm_wifi.setName("SolMon WiFi Signal");
   sm_wifi.setIcon("mdi:wifi");
   sm_wifi.setDeviceClass("signal_strength");
   sm_wifi.setUnitOfMeasurement("dB");
@@ -241,7 +254,7 @@ void setup() {
   sm_log.setIcon("mdi:file-alert");
   sm_mercury.setName("Air Pressure");
   sm_mercury.setIcon("mdi:gauge-low");
-  sm_mercury.setDeviceClass("pressure");
+  sm_mercury.setUnitOfMeasurement("in Hg");
   sm_mbar.setName("Outdoor Air Pressure");
   sm_mbar.setIcon("mdi:gauge-low");
   sm_mbar.setDeviceClass("pressure");
@@ -249,11 +262,11 @@ void setup() {
   sm_c.setName("Outdoor Temperature");
   sm_c.setIcon("mdi:thermometer");
   sm_c.setDeviceClass("temperature");
-  sm_c.setUnitOfMeasurement("°C");
+  sm_c.setUnitOfMeasurement("C");
   sm_f.setName("Outdoor Temperature");
   sm_f.setIcon("mdi:thermometer");
   sm_f.setDeviceClass("temperature");
-  sm_f.setUnitOfMeasurement("°F");
+  sm_f.setUnitOfMeasurement("F");
   sm_humid.setName("Outdoor Humidity");
   sm_humid.setIcon("mdi:gauge");
   sm_humid.setDeviceClass("humidity");
@@ -262,14 +275,106 @@ void setup() {
   mqtt.begin(mqtt_server);
   #endif
 
+  #if defined(OUTPUT_INFLUX)
+  // Sensor parameters
+  wifi_sensor.addTag("device", DEVICE_NAME);
+  wifi_sensor.addTag("manufaturer", MANUFACTURER);
+  wifi_sensor.addTag("model", MODEL_NO);
+  wifi_sensor.addTag("firmware", FW_VERSION);
+  wifi_sensor.addTag("sensor", "WiFi");
+  wifi_sensor.addTag("type", "wifi_signal");
+  wifi_sensor.addTag("unit", "dB");
+  wifi_sensor.addTag("location", "front yard");
+  run_sensor.addTag("device", DEVICE_NAME);
+  run_sensor.addTag("manufaturer", MANUFACTURER);
+  run_sensor.addTag("model", MODEL_NO);
+  run_sensor.addTag("firmware", FW_VERSION);
+  run_sensor.addTag("sensor", "wakeup_counter");
+  run_sensor.addTag("type", "counter");
+  vcc_sensor.addTag("device", DEVICE_NAME);
+  vcc_sensor.addTag("manufaturer", MANUFACTURER);
+  vcc_sensor.addTag("model", MODEL_NO);
+  vcc_sensor.addTag("firmware", FW_VERSION);
+  vcc_sensor.addTag("sensor", "esp_adc");
+  vcc_sensor.addTag("type", "internal_voltage");
+  vcc_sensor.addTag("unit", "Volts");
+  timer_sensor.addTag("device", DEVICE_NAME);
+  timer_sensor.addTag("manufaturer", MANUFACTURER);
+  timer_sensor.addTag("model", MODEL_NO);
+  timer_sensor.addTag("firmware", FW_VERSION);
+  timer_sensor.addTag("sensor", "time_awake");
+  timer_sensor.addTag("type", "timer");
+  timer_sensor.addTag("unit", "milliseconds");
+  battery_sensor.addTag("device", DEVICE_NAME);
+  battery_sensor.addTag("manufaturer", MANUFACTURER);
+  battery_sensor.addTag("model", MODEL_NO);
+  battery_sensor.addTag("firmware", FW_VERSION);
+  battery_sensor.addTag("sensor", "ads1115-0");
+  battery_sensor.addTag("type", "battery_voltage");
+  battery_sensor.addTag("unit", "Volts");
+  battery_sensor.addTag("location", "front yard");
+  panel_sensor.addTag("device", DEVICE_NAME);
+  panel_sensor.addTag("manufaturer", MANUFACTURER);
+  panel_sensor.addTag("model", MODEL_NO);
+  panel_sensor.addTag("firmware", FW_VERSION);
+  panel_sensor.addTag("sensor", "ads1115-1");
+  panel_sensor.addTag("type", "panel_voltage");
+  panel_sensor.addTag("unit", "Volts");
+  panel_sensor.addTag("location", "front yard");
+  log_sensor.addTag("device", DEVICE_NAME);
+  log_sensor.addTag("manufaturer", MANUFACTURER);
+  log_sensor.addTag("model", MODEL_NO);
+  log_sensor.addTag("firmware", FW_VERSION);
+  log_sensor.addTag("type", "log");
+  mbar_sensor.addTag("device", DEVICE_NAME);
+  mbar_sensor.addTag("manufaturer", MANUFACTURER);
+  mbar_sensor.addTag("model", MODEL_NO);
+  mbar_sensor.addTag("firmware", FW_VERSION);
+  mbar_sensor.addTag("sensor", "bme280");
+  mbar_sensor.addTag("type", "air_pressure");
+  mbar_sensor.addTag("unit", "millibars");
+  mbar_sensor.addTag("location", "front yard");
+  mercury_sensor.addTag("device", DEVICE_NAME);
+  mercury_sensor.addTag("manufaturer", MANUFACTURER);
+  mercury_sensor.addTag("model", MODEL_NO);
+  mercury_sensor.addTag("firmware", FW_VERSION);
+  mercury_sensor.addTag("sensor", "bme280");
+  mercury_sensor.addTag("type", "air_pressure");
+  mercury_sensor.addTag("unit", "in_Hg");
+  mercury_sensor.addTag("location", "front yard");
+  celsius_sensor.addTag("device", DEVICE_NAME);
+  celsius_sensor.addTag("manufaturer", MANUFACTURER);
+  celsius_sensor.addTag("model", MODEL_NO);
+  celsius_sensor.addTag("firmware", FW_VERSION);
+  celsius_sensor.addTag("sensor", "bme280");
+  celsius_sensor.addTag("type", "temperature");
+  celsius_sensor.addTag("unit", "C");
+  celsius_sensor.addTag("location", "front yard");
+  temp_sensor.addTag("device", DEVICE_NAME);
+  temp_sensor.addTag("manufaturer", MANUFACTURER);
+  temp_sensor.addTag("model", MODEL_NO);
+  temp_sensor.addTag("firmware", FW_VERSION);
+  temp_sensor.addTag("sensor", "bme280");
+  temp_sensor.addTag("type", "temperature");
+  temp_sensor.addTag("unit", "F");
+  temp_sensor.addTag("location", "front yard");
+  humid_sensor.addTag("device", DEVICE_NAME);
+  humid_sensor.addTag("manufaturer", MANUFACTURER);
+  humid_sensor.addTag("model", MODEL_NO);
+  humid_sensor.addTag("firmware", FW_VERSION);
+  humid_sensor.addTag("sensor", "bme280");
+  humid_sensor.addTag("type", "humidity");
+  humid_sensor.addTag("unit", "%");
+  humid_sensor.addTag("location", "front yard");
+  #endif
 }
 
 void loop() {
-
+  #if defined(OUTPUT_HA)
   mqtt.loop();
+  #endif
   reportStatus();
   reportClimate();
-  reportVoltage();
   digitalWrite(4, LOW);    // Power off peripherals.
   reportWaketime();        // report time spent awake.
   ESP.deepSleep(60e6);
@@ -294,7 +399,15 @@ void reportClimate() {
     Serial.println("could not start measurement, is a measurement already running?");
     #if defined(OUTPUT_HA)
     sm_log.setValue("BMx280 Failure.");
-    #endif  
+    #endif
+    #if defined(OUTPUT_INFLUX)
+    log_sensor.clearFields();
+    log_sensor.addField("log", "BMx280 Failure");
+    if (!client.writePoint(log_sensor)) {
+      Serial.print("InfluxDB write failed: ");
+      Serial.println(client.getLastErrorMessage());
+    }
+    #endif
   }
 
   //wait for the measurement to finish
@@ -314,20 +427,52 @@ void reportClimate() {
   #if defined(OUTPUT_HA)
   sm_mbar.setValue(mbars);
   #endif
+  #if defined(OUTPUT_INFLUX)
+  mbar_sensor.clearFields();
+  mbar_sensor.addField("value", mbars);
+  if (!client.writePoint(mbar_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  #endif
 
   float inHg = mbars / 33.8639;
   #if defined(OUTPUT_HA)
   sm_mercury.setValue(inHg);
+  #endif
+  #if defined(OUTPUT_INFLUX)
+  mercury_sensor.clearFields();
+  mercury_sensor.addField("value",inHg );
+  if (!client.writePoint(mercury_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
   #endif
 
   float celsius = bmx280.getTemperature();
   #if defined(OUTPUT_HA)
   sm_c.setValue(celsius);
   #endif
+  #if defined(OUTPUT_INFLUX)
+  celsius_sensor.clearFields();
+  celsius_sensor.addField("value", celsius);
+  if (!client.writePoint(celsius_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  #endif
 
   float tempF = (celsius * 1.8) + 32;
   #if defined(OUTPUT_HA)
   sm_f.setValue(tempF);
+  #endif
+  #if defined(OUTPUT_INFLUX)
+  temp_sensor.clearFields();
+  temp_sensor.addField("value", tempF);
+  if (!client.writePoint(temp_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
   #endif
 
   if (bmx280.isBME280())
@@ -336,8 +481,17 @@ void reportClimate() {
     Serial.print("Humidity: ");
     #if defined(OUTPUT_HA)
     sm_humid.setValue(humid);
-    #endif 
+    #endif
     Serial.println(humid);
+    #if defined(OUTPUT_INFLUX)
+    humid_sensor.clearFields();
+    humid_sensor.addField("value", humid);
+    if (!client.writePoint(humid_sensor)) {
+      Serial.print("InfluxDB write failed: ");
+      Serial.println(client.getLastErrorMessage());
+    }
+    #endif
+
   }
 }
 
@@ -346,57 +500,81 @@ void reportStatus() {
   static float SOLvolts = (readChannel(ADS1115_COMP_1_GND) * 4);
   static float sigLevel = WiFi.RSSI();
   static float INPUTvolts = ((espVoltage - 220) / 1000);
-  //char value[6];
 
-  //dtostrf(INPUTvolts, 4, 2, value);  // convert volts to 4 character value[] - rounding at 2 decimal spaces
-  //strcat(value, "\0");
   Serial.print("ESP Vcc: ");
   #if defined(OUTPUT_HA)
   sm_vcc.setValue(INPUTvolts);
-  #endif 
+  #endif
   Serial.print(INPUTvolts);
   Serial.println(" volts");
-  
-  //uint32_t resetCount -- global
-  //BATvolts = (readChannel(ADS1115_COMP_0_GND) * 3.26);
-  //SOLvolts = (readChannel(ADS1115_COMP_1_GND) * 4);
+  #if defined(OUTPUT_INFLUX)
+  vcc_sensor.clearFields();
+  vcc_sensor.addField("value", INPUTvolts);
+  if (!client.writePoint(vcc_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  #endif
+
 
   Serial.print(F("\nReset count = "));
   #if defined(OUTPUT_HA)
   sm_run.setValue(resetCount);
-  #endif 
+  #endif
   Serial.println(resetCount);
+  #if defined(OUTPUT_INFLUX)
+  run_sensor.clearFields();
+  run_sensor.addField("value", resetCount);
+  if (!client.writePoint(run_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  #endif
+
   
   Serial.print("Battery: ");
   #if defined(OUTPUT_HA)
   sm_bat.setValue(BATvolts);
-  #endif 
+  #endif
   Serial.println(BATvolts);
+  #if defined(OUTPUT_INFLUX)
+  battery_sensor.clearFields();
+  battery_sensor.addField("value", BATvolts);
+  if (!client.writePoint(battery_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  #endif
+
 
   Serial.print("Panel:   ");
   #if defined(OUTPUT_HA)
   sm_sol.setValue(SOLvolts);
-  #endif 
+  #endif
   Serial.println(SOLvolts);
+  #if defined(OUTPUT_INFLUX)
+  panel_sensor.clearFields();
+  panel_sensor.addField("value", SOLvolts);
+  if (!client.writePoint(panel_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  #endif
+
 
   Serial.print("WiFi dB: ");
   #if defined(OUTPUT_HA)
   sm_wifi.setValue(sigLevel);
   #endif 
   Serial.println(sigLevel);
-}
-
-void reportVoltage() {            // read internal VCC
-  static float INPUTvolts = ((espVoltage - 220) / 1000);
-  char value[6];
-  dtostrf(INPUTvolts, 4, 2, value);  // convert volts to 4 character value[] - rounding at 2 decimal spaces
-  strcat(value, "\0");
-  Serial.print("ESP Vcc: ");
-  #if defined(OUTPUT_HA)
-  sm_vcc.setValue(INPUTvolts);
-  #endif 
-  Serial.print(value);
-  Serial.println(" volts");
+  #if defined(OUTPUT_INFLUX)
+  wifi_sensor.clearFields();
+  wifi_sensor.addField("value", sigLevel);
+  if (!client.writePoint(wifi_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  #endif
 }
 
 void reportWaketime() {
@@ -404,6 +582,14 @@ void reportWaketime() {
   #if defined(OUTPUT_HA)
   int timer = millis();
   sm_time.setValue(timer);
+  #endif
+  #if defined(OUTPUT_INFLUX)
+  timer_sensor.clearFields();
+  timer_sensor.addField("value", millis());
+  if (!client.writePoint(timer_sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
   #endif
   Serial.println(millis());
   Serial.flush();
@@ -413,8 +599,10 @@ void updateRTCcrc() {  // updates the reset count CRC
   nv->rtcData.crc32 = crc32((uint8_t*) &nv->rtcData.rstCount, sizeof(nv->rtcData.rstCount));
 }
 
+/*
 void onBeforeSwitchStateChanged(bool state, HASwitch* s)
 {
     // this callback will be called before publishing new state to HA
     // in some cases there may be delay before onStateChanged is called due to network latency
 }
+*/
